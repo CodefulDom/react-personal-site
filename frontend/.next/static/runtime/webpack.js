5a610a7cd045e502fd7b8bd56810001423228268
codefuldom <dom@codefuldom.com>
2020-01-18 22:07:37 -0600

    :heavy_plus_sign: add deps for fontawesome

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3f34d72406aab2223264";
+/******/ 	var hotCurrentHash = "8bc6caba7cdf50a556b2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ frontend/pages/uses.js
@@ -0,0 +1,9 @@
+const Uses = () => {
+  return (
+    <div>
+      <h1>Uses</h1>
+    </div>
+  )
+}
+
+export default Uses
